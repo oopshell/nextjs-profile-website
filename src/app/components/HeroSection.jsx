e7e5eb2b8cf7a98f92555c09053f7faaeb88b2eb
Tiantian Li <tiantiancscs@gmail.com>
2024-10-28 11:12:38 +1100

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -22,13 +22,13 @@ const HeroSection = () => {
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
+                "Tiantian Li",
                 1000,
-                "Web Developer",
+                "Software Engineer",
                 1000,
-                "Mobile Developer",
-                1000,
-                "UI/UX Designer",
+                // "FullStack Developer",
+                // 1000,
+                "Data Scientist",
                 1000,
               ]}
               wrapper="span"
@@ -37,8 +37,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
+            Click the tags in the navigation bar to learn more about me and my journey. Dive into my projects, explore my experiences, and connect with my passions. Happy browsing!
           </p>
           <div>
             <Link
