d6774e90c6dba23dcc1f00e235dd1a7378955de8
Tiantian Li <tiantiancscs@gmail.com>
2024-10-29 23:05:47 +1100

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -24,10 +24,8 @@ const HeroSection = () => {
               sequence={[
                 "Tiantian Li",
                 1000,
-                "Software Engineer",
+                "FullStack Developer", // "Software Engineer",
                 1000,
-                // "FullStack Developer",
-                // 1000,
                 "Data Scientist",
                 1000,
               ]}
@@ -47,7 +45,8 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/"
+              href="https://tiantian-li.me/resume-tiantian_li.pdf"
+              target="_blank" rel="noopener noreferrer"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
@@ -62,11 +61,12 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] overflow-hidden relative">
             <Image
               src="/images/hero-image.png"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              // className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              className="absolute w-full h-full object-cover top-0 left-0"
               width={300}
               height={300}
             />
