549ec6258980222dda1a7ea4cd8fcfdb9cac38bd
Tiantian Li <tiantiancscs@gmail.com>
2024-10-29 23:05:45 +1100

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,8 +4,10 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span>LOGO</span>
-        <p className="text-slate-600">All rights reserved.</p>
+        <span>
+          <img src="logo.svg" alt="Logo" className="h-10 w-10 bg-transparent rounded-full" />
+        </span>
+        <p className="text-slate-600">&copy; 2024 My Portfolio. All rights reserved.</p>
       </div>
     </footer>
   );
